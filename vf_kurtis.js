77cdd2e2598d73cc97093729d506b653819f0cbc
speed <speedjudy928@gmail.com>
2023-02-25 20:06:06 +0900

    fix

--- vf_kurtis.js
+++ vf_kurtis.js
@@ -36,8 +36,8 @@ let SpeechRecognition =
   window.SpeechRecognition;
 speechRecognition = new SpeechRecognition();
 speechRecognition.lang = "en-GB";
-speechRecognition.continuous = false;
-speechRecognition.interimResults = false;
+speechRecognition.continuous = true;
+speechRecognition.interimResults = true;
 speechRecognition.maxAlternatives = 1;
 
 let overlay = document.createElement("div");
